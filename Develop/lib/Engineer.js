bbceab0c07ac173e83b3f848b418794b986dc0ab
Dibello80 <angelodibellomusic@gmail.com>
2020-11-30 00:02:30 -0800

    libcodes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+// Engineer class that creates an object that extends the Employee class and adds engineer specific parameters
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    };
+    getGithub() {
+        return this.github;
+    };
+    getRole() {
+        return "Engineer";
+    };
+};
+
+module.exports = Engineer;
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+  
+const Employee = require('./Employee');
+
+// Intern class that creates an object that extends the Employee class and adds intern specific parameters
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email);
+        this.school = school;
+    };
+    getSchool() {
+        return this.school;
+    };
+    getRole() {
+        return "Intern";
+    };
+};
+
+module.exports = Intern;
